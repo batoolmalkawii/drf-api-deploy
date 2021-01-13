17e7aaafbc50f86ba94393d633fe4a2a745dfd14
Batool Malkawi <batoolmalkawii@gmail.com>
2021-01-13 16:52:12 +0200

    Added Deployment

--- libraryapi_project/settings.py
+++ libraryapi_project/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+    DEBUG=(bool,False)
+)
+
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3vd6xqp_$ezg4&sd19j+#6e4a63ji=6of1(pfcx$(=et)zbdv8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 
 # Application definition
@@ -85,12 +93,12 @@ WSGI_APPLICATION = 'libraryapi_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': ('django.db.backends.postgresql'),
-        'NAME':  ('cudivtnx'),
-        'USER': ('cudivtnx'),
-        'PASSWORD': ('9SzhipL1s7_K9Vcv4RrZ9_uOiIn-9plH'),
-        'HOST': ('ziggy.db.elephantsql.com'),
-        'PORT': ('5432'),
+        'ENGINE': env('DATABASE_ENGINE'),
+        'NAME':  env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
